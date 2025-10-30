da3a698d3687e167ca42918dbd25c774c09785e5
Pavel <p0chta3@yandex.ru>
2025-10-31 01:04:55 +0800

    Create Docker files and docker-compose

---
+++ backend/entrypoint.sh
@@ -0,0 +1,6 @@
+python manage.py makemigrations --no-input
+python manage.py migrate --no-input
+python manage.py collectstatic --no-input
+mkdir /app/static/backend-static/docs/
+cp -r /app/api/docs /app/static/
+gunicorn --bind 0.0.0.0:8000 foodgram_backend.wsgi
