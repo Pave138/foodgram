becde50ba176b913755aeef3900ae0c8cfd971cc
Pavel <p0chta3@yandex.ru>
2025-11-01 14:52:00 +0800

    Update.

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -22,7 +22,7 @@ DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(', ')
 
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '')
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(', ')
 
 AUTH_USER_MODEL = 'users.User'
 
