19e280175280d47df767eb95c1b237197b821d7b
Pavel <p0chta3@yandex.ru>
2025-11-01 13:16:57 +0800

    Update.

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,5 @@
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
-mkdir /app/static/docs/
-cp -r /app/api/docs /app/static/docs
+cp -r /app/api/docs /app/static/
 gunicorn --bind 0.0.0.0:8000 foodgram_backend.wsgi
